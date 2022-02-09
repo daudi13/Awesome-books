852e77f299c9f04630a75417138f0a0408d53230
i-max-xi <appiahmax4u@gmail.com>
2022-02-09 13:31:45 +0000

    Fix eslint quotes

--- JS/main.js
+++ JS/main.js
@@ -1,30 +1,30 @@
 const books = [];
-const listSection = document.querySelector(".list-section");
-const bookTitle = document.querySelector("#title");
-const bookAuthor = document.querySelector("#author");
-const theForm = document.querySelector("form");
+const listSection = document.querySelector('.list-section');
+const bookTitle = document.querySelector('#title');
+const bookAuthor = document.querySelector('#author');
+const theForm = document.querySelector('form');
 
 // adding items in the books array
 
 function addBookItem(bookItem) {
-  const bookBlock = document.createElement("div");
-  bookBlock.classList.add("bookBlock");
+  const bookBlock = document.createElement('div');
+  bookBlock.classList.add('bookBlock');
 
-  const removeBtn = document.createElement("button");
-  removeBtn.classList.add("remove-btn");
-  removeBtn.innerText = "remove";
+  const removeBtn = document.createElement('button');
+  removeBtn.classList.add('remove-btn');
+  removeBtn.innerText = 'remove';
 
-  const underLine = document.createElement("hr");
+  const underLine = document.createElement('hr');
 
-  bookBlock.innerHTML = `<p class="book-title">${bookItem.title}</p>
-<p class="book-Author">${bookItem.author}</p>`;
+  bookBlock.innerHTML = `<p class='book-title'>${bookItem.title}</p>
+<p class='book-Author'>${bookItem.author}</p>`;
   bookBlock.appendChild(removeBtn);
   bookBlock.appendChild(underLine);
   listSection.appendChild(bookBlock);
-  const x = Array.from(document.querySelectorAll(".bookBlock"));
+  const x = Array.from(document.querySelectorAll('.bookBlock'));
 
-  document.querySelectorAll(".remove-btn").forEach((btn, i) => {
-    btn.addEventListener("click", () => {
+  document.querySelectorAll('.remove-btn').forEach((btn, i) => {
+    btn.addEventListener('click', () => {
       x[i].remove();
     });
   });
@@ -37,12 +37,12 @@ function addBooks(item) {
     author: bookAuthor.value,
   });
 
-  localStorage.setItem("CollectedBooks", JSON.stringify(books));
-  bookTitle.value = "";
-  bookAuthor.value = "";
+  localStorage.setItem('CollectedBooks', JSON.stringify(books));
+  bookTitle.value = '';
+  bookAuthor.value = '';
 }
 
-theForm.addEventListener("submit", (e) => {
+theForm.addEventListener('submit', (e) => {
   e.preventDefault();
   addBooks({
     title: bookTitle.value,
