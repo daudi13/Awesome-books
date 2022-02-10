b59b0c5496816f2e8835504397a6a0f6609899a0
daudi13 <davouma13@gmail.com>
2022-02-10 05:40:04 +0300

    Add addBookItem function

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,7 @@
-
-
-
 // Initialize the books
 let books;
 
-
 // create the book class
-
 class Book {
 	constructor(author, title) {
 		this.title = title
@@ -15,7 +10,7 @@ class Book {
 }
 
 class updateDisplay {
-	static listSection = document.querySelector('container')
+	static listSection = document.querySelector('.list-section');
 	static bookTitle = document.querySelector('#title');
 	static theForm = document.querySelector('form');
 	static bookAuthor = document.querySelector('#author');
@@ -26,8 +21,34 @@ class updateDisplay {
 
 		books.push(bookItem);
 		localStorage.setItem('collection', JSON.stringify(books));
-
 		updateDisplay.bookAuthor.value = '';
 		updateDisplay.bookTitle.value = '';
 	}
+
+	static addBookItem(bookItem) {
+	const bookBlock = document.createElement('div');
+	bookBlock.classList.add('bookBlock');
+
+	const removeBtn = document.createElement('button');
+	removeBtn.classList.add('remove-btn');
+	removeBtn.innerText = 'remove';
+
+	const underLine = document.createElement('hr');
+
+	bookBlock.innerHTML = `<p class="book-title">${bookItem.title}</p>
+			<p class="book-Author">${bookItem.author}</p>`
+			bookBlock.appendChild(removeBtn);
+			bookBlock.appendChild(underLine);
+			updateDisplay.listSection.appendChild(bookBlock);
+	let x = Array.from(document.querySelectorAll('.bookBlock'));
+
+	document.querySelectorAll('.remove-btn').forEach((btn, i) => {
+		btn.addEventListener('click', () => {
+			x[i].remove();
+		})
+	})
 }
+}
+
+
+updateDisplay.theForm.addEventListener('submit', updateDisplay.addBookItem);
