31ba44abca8389f44d166ac5fd0b571ec2378aab
i-max-xi <appiahmax4u@gmail.com>
2022-02-11 13:49:22 +0000

    Fix eslint

--- JS/main.js
+++ JS/main.js
@@ -15,84 +15,87 @@ class UpdateDisplay {
 
   static formBtn = document.querySelector('.btn-submit');
 
-static bookAuthor = document.querySelector('#author');
+  static bookAuthor = document.querySelector('#author');
 
-static listBtn = document.querySelectorAll('.listBtn');
+  static listBtn = document.querySelectorAll('.listBtn');
 
-// static date = document.querySelector('')
+  // static date = document.querySelector('')
 
-static thedate = new Date();
+  static thedate = new Date();
 
-static options = {
-month: 'long',
-day: 'numeric',
-year: 'numeric',
-hour: 'numeric',
-minute: 'numeric',
-second: 'numeric',
-}
+  static options = {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  };
 
-static date = document.querySelector('.date');
+  static date = document.querySelector('.date');
 
-static addActive = document.querySelectorAll('.sec');
+  static addActive = document.querySelectorAll('.sec');
 
-static timeUpdate() {
-return UpdateDisplay.thedate.toLocaleDateString('en-US', UpdateDisplay.options);
-}
+  static timeUpdate() {
+    return UpdateDisplay.thedate.toLocaleDateString(
+      'en-US',
+      UpdateDisplay.options
+    );
+  }
 
-// create new book
-static addBooks() {
-const bookItem = new UpdateDisplay(
-	UpdateDisplay.bookTitle.value,
-	    UpdateDisplay.bookAuthor.value,
-	  );
-	  books.push(bookItem);
-	  localStorage.setItem('books', JSON.stringify(books));
+  // create new book
+  static addBooks() {
+    const bookItem = new UpdateDisplay(
+      UpdateDisplay.bookTitle.value,
+      UpdateDisplay.bookAuthor.value
+    );
+    books.push(bookItem);
+    localStorage.setItem('books', JSON.stringify(books));
 
-	  UpdateDisplay.bookAuthor.value = '';
-	  UpdateDisplay.bookTitle.value = '';
-	  UpdateDisplay.addBookItem(bookItem, books.length - 1);
-	}
+    UpdateDisplay.bookAuthor.value = '';
+    UpdateDisplay.bookTitle.value = '';
+    UpdateDisplay.addBookItem(bookItem, books.length - 1);
+  }
 
-	static delBook(bookItem, pos) {
-	  const bookBlock = document.getElementById(pos);
-	  books = books.filter((item) => item !== bookItem);
-	  localStorage.setItem('books', JSON.stringify(books));
-	  UpdateDisplay.listSection.removeChild(bookBlock);
-	}
+  static delBook(bookItem, pos) {
+    const bookBlock = document.getElementById(pos);
+    books = books.filter((item) => item !== bookItem);
+    localStorage.setItem('books', JSON.stringify(books));
+    UpdateDisplay.listSection.removeChild(bookBlock);
+  }
 
-	static updateUi() {
-	  if (localStorage.getItem('books')) {
-	    books = JSON.parse(localStorage.getItem('books'));
-	    books.forEach((bookItem, pos) => {
-	      UpdateDisplay.addBookItem(bookItem, pos);
-	    });
-	  } else {
-	    localStorage.setItem('books', '');
-	    books = [];
-	  }
-	}
+  static updateUi() {
+    if (localStorage.getItem('books')) {
+      books = JSON.parse(localStorage.getItem('books'));
+      books.forEach((bookItem, pos) => {
+        UpdateDisplay.addBookItem(bookItem, pos);
+      });
+    } else {
+      localStorage.setItem('books', '');
+      books = [];
+    }
+  }
 
-	static addBookItem(bookItem, pos) {
-	  const bookBlock = document.createElement('div');
-	  bookBlock.classList.add('bookBlock');
-	  bookBlock.id = pos;
+  static addBookItem(bookItem, pos) {
+    const bookBlock = document.createElement('div');
+    bookBlock.classList.add('bookBlock');
+    bookBlock.id = pos;
 
-	  const removeBtn = document.createElement('button');
-	  removeBtn.classList.add('remove-btn');
+    const removeBtn = document.createElement('button');
+    removeBtn.classList.add('remove-btn');
 
-	  bookBlock.innerHTML = `
+    bookBlock.innerHTML = `
       <p class='book-title'>'${bookItem.author}  by ${bookItem.title}</p>`;
 
-	  removeBtn.innerText = 'remove';
+    removeBtn.innerText = 'remove';
 
-	  removeBtn.onclick = () => {
-	    UpdateDisplay.delBook(bookItem, pos);
-	  };
+    removeBtn.onclick = () => {
+      UpdateDisplay.delBook(bookItem, pos);
+    };
 
-	  bookBlock.appendChild(removeBtn);
-	  UpdateDisplay.listSection.appendChild(bookBlock);
-	}
+    bookBlock.appendChild(removeBtn);
+    UpdateDisplay.listSection.appendChild(bookBlock);
+  }
 }
 
 UpdateDisplay.listBtn.forEach((btn, i) => {
@@ -107,7 +110,10 @@ UpdateDisplay.listBtn.forEach((btn, i) => {
   };
 });
 
-UpdateDisplay.date.innerText = UpdateDisplay.thedate.toLocaleDateString('en-US', UpdateDisplay.options);
+UpdateDisplay.date.innerText = UpdateDisplay.thedate.toLocaleDateString(
+  'en-US',
+  UpdateDisplay.options
+);
 
 UpdateDisplay.updateUi();
 
